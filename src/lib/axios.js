591680c4c6eae6a1589aaa28c682fbf867a51233
Shashikr36 <137677581+Shashikr36@users.noreply.github.com>
2026-06-22 02:44:42 +0530

    Update axios baseURL to use VITE_API_URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.NODE_ENV === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
