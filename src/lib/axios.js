e93ace78b69fd4035638297b1d93aff700bce382
Shashikr36 <shashikrpatwa36@gmail.com>
2026-06-22 02:20:10 +0530

    update-axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,18 +1,7 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.NODE_ENV === "development" ? "http://localhost:5001/api" : "/api",
-//   withCredentials: true,
-// });
-
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chat-application-backend-4ban.onrender.com/api",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: import.meta.env.NODE_ENV === "development" ? "http://localhost:5001/api" : "/api",
+  withCredentials: true,
 });
 
