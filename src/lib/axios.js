ea85e767db76d67ddc3de1c80baa85f0cffecf12
Shashikr36 <shashikrpatwa36@gmail.com>
2026-06-24 21:50:03 +0530

    updated Base url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: "http://127.0.0.1:5001/api",
   withCredentials: true,
 });
